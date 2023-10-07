66e5ff2c8ddb8e8f991aa1d857187f151d92c7c3
mahaalmas <maha.almasoudi@students.plymouth.ac.uk>
2023-10-07 12:22:18 +0100

    Update 101-HelloWorld.cpp
    Task 1 by Maha Almasoudi

--- Tasks/01 - Variables and Arithmetic/101-HelloWorld/101-HelloWorld.cpp
+++ Tasks/01 - Variables and Arithmetic/101-HelloWorld/101-HelloWorld.cpp
@@ -1,5 +1,5 @@
 // 101-HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//by Maha M Almasoudi-10876192
 
 //This is known as a "header file" - we will look at this later
 #include <stdio.h>
@@ -7,17 +7,9 @@
 //This is the entry point to our code - known as the "main function" - we will meet functions later
 int main()
 {
-    //Write Hello World to the terminal with puts  ("put string")
+    puts("************");
     puts("Hello World");
-
-    /*
-        TASK: Add two more lines of code to the above so that the output reads as follows:
-
-        ***********
-        Hello World
-        ***********
-        
-    */ 
+    puts("************");
 }
 
 
